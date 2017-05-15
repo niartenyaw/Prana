56266e872dec4735b241fa52d5f8d00cd50ede09
Aaron Wayne <amwayne90@gmail.com>
2017-05-14 22:26:33 -0700

    Update gemfile and setup npm

---
+++ frontend/prana.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById('root');
+
+  ReactDOM.render(<h1>Prana coming soon</h1>, root);
+});
