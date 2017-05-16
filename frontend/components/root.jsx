8abf3c45b7c53d9bdb2b477af66bca247094370c
Aaron Wayne <amwayne90@gmail.com>
2017-05-15 22:13:58 -0700

    Add root and render through entry

---
+++ frontend/components/root.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { HashRouter } from 'react-router-dom';
+
+const Root = () => (
+  <h1>Root!</h1>
+);
+
+export default Root;
--- frontend/prana.jsx
+++ frontend/prana.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
+import Root from './components/root';
 
 // Temp
-import {signin, signout, signup} from './actions/session_actions';
-window.signin = signin;
-window.signout = signout;
-window.signup = signup;
+// import {signin, signout, signup} from './actions/session_actions';
+// window.signin = signin;
+// window.signout = signout;
+// window.signup = signup;
 
 
 
 document.addEventListener("DOMContentLoaded", () => {
   const store = configureStore();
-  window.store = store;
+  // window.store = store;
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Prana coming soon</h1>, root);
+  ReactDOM.render(<Root />, root);
 });
