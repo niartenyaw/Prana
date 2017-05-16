4ab6002b1c30ecbd983ecf1833b5c4e74b702175
Aaron Wayne <amwayne90@gmail.com>
2017-05-15 17:33:18 -0700

    Complete User model

---
+++ app/models/user.rb
@@ -0,0 +1,47 @@
+class User < ApplicationRecord
+  validates :email, :session_token, presence: true, uniqueness: true
+  validates :password_digest, :name, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+  validate :email_must_be_valid
+  after_initialize :ensure_session_token
+
+  attr_reader :password
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+    if user.is_password?(password)
+      return user
+    end
+
+    nil
+  end
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64(16)
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64(16)
+    self.save
+
+    self.session_token
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def password= (password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  private
+
+  def email_must_be_valid
+    unless self.email =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+      errors[:email] << "Invalid email address"
+    end
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
 
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  get 'users/create'
+
+  get 'users/destroy'
+
   root to: 'static_pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
