90d8ec226c1fabd5452b975d1951f227b04c25b2
Aaron Wayne <amwayne90@gmail.com>
2017-05-17 10:17:15 -0700

    More contrast to hero img and aligning of hero text

--- frontend/components/session/session_form.jsx
+++ frontend/components/session/session_form.jsx
@@ -33,34 +33,36 @@ class SessionForm extends React.Component {
     const path = this.props.match.path
     const text = path === "/signin" ? "Sign In" : "Sign Up";
     return (
-      <div className="form-container">
+      <div className="hero-container">
         <section className="hero-text">
           <h1>Move Work Forward</h1>
           <h4>Prana is the easiest way for teams to track their work—and get results.</h4>
         </section>
-        <form className="session-form" onSubmit={e => this.handleSubmit(e)}>
-          <div className="form-header">
-            <h3 id="form-title">{text}</h3>
-          </div>
-          {(path === "/signup") ? (<label>Name
-            <input type="text"
-              value={this.state.name}
-              onChange={this.handleChange("name")} ></input>
-          </label>) : (
-            ""
-          )}
-          <label>Email
-            <input type="text"
-              value={this.state.email}
-              onChange={this.handleChange("email")} ></input>
-          </label>
-          <label>Password
-            <input type="password"
-              value={this.state.password}
-              onChange={this.handleChange("password")} ></input>
-          </label>
-          <input className="button" type="submit" value={text} />
-        </form>
+        <section className="form-container">
+          <form className="session-form" onSubmit={e => this.handleSubmit(e)}>
+            <div className="form-header">
+              <h3 id="form-title">{text}</h3>
+            </div>
+            {(path === "/signup") ? (<label>Name
+              <input type="text"
+                value={this.state.name}
+                onChange={this.handleChange("name")} ></input>
+            </label>) : (
+              ""
+            )}
+            <label>Email
+              <input type="text"
+                value={this.state.email}
+                onChange={this.handleChange("email")} ></input>
+            </label>
+            <label>Password
+              <input type="password"
+                value={this.state.password}
+                onChange={this.handleChange("password")} ></input>
+            </label>
+            <input className="button" type="submit" value={text} />
+          </form>
+        </section>
       </div>
     );
   }
