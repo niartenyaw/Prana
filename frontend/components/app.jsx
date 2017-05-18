810f14c0e62fa205a6f7d8f96eb9143f4c6063d0
Aaron Wayne <amwayne90@gmail.com>
2017-05-18 16:06:41 -0700

    Get ready for heroku

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -5,7 +5,6 @@ import { AuthRoute, ProRoute } from '../util/route_util';
 import SplashContainer from './splash/splash_container';
 import TopNavContainer from './top_nav/top_nav_container';
 import SideNavContainer from './side_nav/side_nav_container';
-import TeamIndexContainer from './team/team_index_container';
 import MainPanelContainer from './main_panel/main_panel_container';
 
 const App = () => (
