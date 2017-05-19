8762cb98a85e38d5e7618a696c9d2dbc96048b18
Aaron Wayne <amwayne90@gmail.com>
2017-05-18 17:10:58 -0700

    Add index and validation to team memberships to guarantee unique pairs

--- app/models/team_membership.rb
+++ app/models/team_membership.rb
@@ -1,5 +1,6 @@
 class TeamMembership < ApplicationRecord
   validates :team, :user, presence: true
+  validates_uniqueness_of :user, scope: :team
 
   belongs_to :user
   belongs_to :team
--- frontend/components/team/team_index.jsx
+++ frontend/components/team/team_index.jsx
@@ -14,6 +14,7 @@ class TeamIndex extends React.Component {
   render() {
     return (
       <section className="teams-index">
+        <h4 className="teams-index-header">Teams</h4>
         <ul>
           {this.props.allTeams.map(team => <TeamIndexItem key={team.id} team={team} />)}
         </ul>
