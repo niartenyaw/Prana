927ce09613367f12509dfe44ef051944576c9782
Aaron Wayne <amwayne90@gmail.com>
2017-05-22 17:30:17 -0700

    Add seeds for projects and memberships

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,25 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-user1 = User.create(email: "guest@guest.com", name: "Guest", password:"password")
-user2 = User.create(email: "2@guest.com", name: "Guest2", password:"password")
-user3 = User.create(email: "3@guest.com", name: "Guest3", password:"password")
+user1 = User.create(email: "guest@guest.com", name: "guest", password:"password")
 
-team1 = Team.create(creator_id: user1.id, name: "Team 1")
-team2 = Team.create(creator_id: user2.id, name: "Team 2")
-team3 = Team.create(creator_id: user3.id, name: "Team 3")
+team1 = Team.create(creator_id: user1.id, name: "team 1")
+team2 = Team.create(creator_id: user1.id, name: "team 2")
+team3 = Team.create(creator_id: user1.id, name: "team 3")
 
-mem1 = TeamMembership.create(user_id: user1.id, team_id: team1.id)
-mem2 = TeamMembership.create(user_id: user1.id, team_id: team2.id)
-mem3 = TeamMembership.create(user_id: user2.id, team_id: team1.id)
-mem4 = TeamMembership.create(user_id: user3.id, team_id: team3.id)
+tmem1 = TeamMembership.create(user_id: user1.id, team_id: team1.id)
+tmem2 = TeamMembership.create(user_id: user1.id, team_id: team2.id)
+tmem4 = TeamMembership.create(user_id: user1.id, team_id: team3.id)
+
+
+project1 = Project.create(name: "project 1", creator_id: user1.id, team_id: team1.id)
+project2 = Project.create(name: "another project", creator_id: user1.id, team_id: team1.id)
+project3 = Project.create(name: "and another one!", creator_id: user1.id, team_id: team1.id)
+project4 = Project.create(name: "lonely project :(", creator_id: user1.id, team_id: team2.id)
+project5 = Project.create(name: "also a lonely project :(", creator_id: user1.id, team_id: team3.id)
+
+pmem1 = ProjectMembership.create(user_id: user1.id, project_id: project1.id)
+pmem1 = ProjectMembership.create(user_id: user1.id, project_id: project2.id)
+pmem1 = ProjectMembership.create(user_id: user1.id, project_id: project3.id)
+pmem1 = ProjectMembership.create(user_id: user1.id, project_id: project4.id)
+pmem1 = ProjectMembership.create(user_id: user1.id, project_id: project5.id)
